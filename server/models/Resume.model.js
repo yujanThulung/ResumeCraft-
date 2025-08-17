a8d6c70c8ee6236b36df95bd2adfdd2c20b65728
yujan Thulung <yujanrai07@gmail.com>
2025-08-17 18:46:18 +0545

    feat: Model is created

---
+++ server/models/Resume.model.js
@@ -0,0 +1,118 @@
+import mongoose from "mongoose";
+
+const resumeSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        personalDetails: {
+            fullName: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+            },
+            phone: {
+                type: String,
+                required: true,
+            },
+            address: {
+                type: String,
+                required: true,
+            },
+            linkedin: {
+                type: String,
+                required: true,
+            },
+            github: {
+                type: String,
+                required: true,
+            },
+            website: {
+                type: String,
+                required: true,
+            },
+            portfolio: {
+                type: String,
+                required: true,
+            },
+        },
+
+        education: [
+            {
+                institution: String,
+                degree: String,
+                fieldOfStudy: String,
+                startDate: Date,
+                endDate: Date,
+                description: String,
+            },
+        ],
+
+        experience: [
+            {
+                company: String,
+                position: String,
+                startDate: Date,
+                endDate: Date,
+                description: String,
+            },
+        ],
+
+        skills: [
+            {
+                skill: String,
+                level: String,
+                description: String,
+            },
+        ],
+
+        languages: [
+            {
+                language: String,
+                level: String,
+                description: String,
+            },
+        ],
+
+        certifications: [
+            {
+                name: String,
+                issuingOrganization: String,
+                issueDate: Date,
+                description: String,
+            },
+        ],
+
+        summary: String,
+
+        template: {
+            type: String,
+            default: "default",
+        },
+
+        extraSections: [
+            {
+                title: {
+                    type: String,
+                    required: true,
+                },
+                content: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Resume = mongoose.model("Resume", resumeSchema);
+export default Resume;
--- server/models/index.model.js
+++ server/models/index.model.js
@@ -1,3 +1,8 @@
 import User from "./User.model.js";
+import Resume from "./Resume.model.js";
 
+
+
+
+export{Resume};
 export default User;
