89da6b68cf2c6e3a917a8c392de8c829b062e459
yujan Thulung <yujanrai07@gmail.com>
2025-08-24 09:23:51 +0545

    feat: PageNotFound page is added

--- client/src/pages/PageNotfoundPage.jsx
+++ client/src/pages/PageNotfoundPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const PageNotfoundPage = () => {
-  return (
-    <div>PageNotfound.page</div>
-  )
-}
+    return (
+        <div className="flex flex-col items-center justify-center h-screen text-gray-900 animate-pulse">
+            <h1 className="text-9xl font-bold text-gray-900 tracking-wider">404</h1>
+            <h2 className="text-4xl font-semibold">Page Not Found</h2>
+        </div>
+    );
+};
 
-export default PageNotfoundPage
+export default PageNotfoundPage;
